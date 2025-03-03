a2e8ac61eefdcfabd9fb7ac65ac3cb0244a31789
asgerdur03 <asgerdur03@gmail.com>
2025-03-03 22:33:08 +0000

    deploy virkar en ekki actions

--- src/slug.js
+++
@@ -1,8 +0,0 @@
-export function generateSlug(title) {
-    return title
-        .toLowerCase() // Convert to lowercase
-        .trim() // Remove extra spaces
-        .replace(/[^a-z0-9\s-]/g, '') // Remove special characters
-        .replace(/\s+/g, '-') // Replace spaces with hyphens
-        .replace(/-+/g, '-'); // Remove duplicate hyphens
-}
